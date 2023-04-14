2a45da5f674ce4f2addab7235d2ebdcdcde8ef5f
Nikolaipe <nikolaipe@getacademy.no>
2023-04-14 14:32:41 +0200

    Add project files.

---
+++ LailaiLand.TreeCutter/KeyPresser.cs
@@ -0,0 +1,16 @@
+﻿namespace LailaiLand.TreeCutter
+{
+    internal class KeyPresser
+    {
+        private ConsoleKeyInfo _key;
+
+        public ConsoleKeyInfo Keypress()
+        {
+            ConsoleKeyInfo keyInfo;
+
+            keyInfo = Console.ReadKey(true);
+            _key = keyInfo;
+            return _key;
+        }
+    }
+}
